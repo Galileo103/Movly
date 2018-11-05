77f582eeaa46269a3caf7440ec0a9358f7132015
Galileo <galal.shaban96@gmail.com>
2018-11-06 01:56:45 +0200

    Build an API for movies and customers

---
+++ Movly/App_Start/MappingProfile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using Movly.Models;
+using Movly.Dtos;
+
+namespace Movly.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Domain to Dto
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<Movie, MovieDto>();
+
+            // Dto to Domain
+            Mapper.CreateMap<CustomerDto, Customer>()
+                .ForMember(c => c.Id, opt => opt.Ignore());
+            Mapper.CreateMap<MovieDto, Movie>()
+               .ForMember(c => c.Id, opt => opt.Ignore());
+
+        }
+    }
+}
---
+++ Movly/Dtos/CustomerDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Movly.Models;
+
+
+namespace Movly.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsletter { get; set; }
+
+        //[MinAgeForChooseMembershipType]
+        public DateTime? Birthdate { get; set; }
+
+        [Required]
+        public byte MembershipTypeId { get; set; }
+    }
+}
