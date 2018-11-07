d86cb609680b1c230a32d8848f35b68ab4f888e6
Galileo <galal.shaban96@gmail.com>
2018-11-07 17:39:31 +0200

    Enable SSL in Movly

--- Movly/App_Start/FilterConfig.cs
+++ Movly/App_Start/FilterConfig.cs
@@ -9,6 +9,7 @@ namespace Movly
         {
             filters.Add(new HandleErrorAttribute());
             filters.Add(new AuthorizeAttribute());
+            filters.Add(new RequireHttpsAttribute());
         }
     }
 }
